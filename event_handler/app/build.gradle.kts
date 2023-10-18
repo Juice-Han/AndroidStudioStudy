913b6db53c4e2f0fd3643bd0affc7f1a9462dab1
JuHan Park <iiiamst16@gmail.com>
2023-10-18 09:03:11 +0900

    study event handler

--- event_handler/app/build.gradle.kts
+++ event_handler/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.event_handler"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.event_handler"
--- event_handler/app/src/main/java/com/example/event_handler/MainActivity.kt
+++ event_handler/app/src/main/java/com/example/event_handler/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.event_handler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton = findViewById<Button>(R.id.button)
+        val lis = myListener()
+        myButton.setOnClickListener(lis)
+    }
+
+    inner class myListener : View.OnClickListener {
+        override fun onClick(p0: View?) {
+            val s : String = "안녕, 안드로이드 12!"
+            var myTextView = findViewById<TextView>(R.id.textView)
+            myTextView.text = s
+        }
     }
 }
