0a95196c4ea46d7a95840d383e631045d9298176
JuHan Park <iiiamst16@gmail.com>
2023-10-18 22:59:18 +0900

    OnClickListener to Lambda

--- event_handler/app/src/main/java/com/example/event_handler/MainActivity.kt
+++ event_handler/app/src/main/java/com/example/event_handler/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.event_handler
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,15 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val myButton = findViewById<Button>(R.id.button)
-        val lis = myListener()
-        myButton.setOnClickListener(lis)
-    }
-
-    inner class myListener : View.OnClickListener {
-        override fun onClick(p0: View?) {
-            val s : String = "안녕, 안드로이드 12!"
+        myButton.setOnClickListener {
+            val s: String = "안녕, 안드로이드 13!"
             var myTextView = findViewById<TextView>(R.id.textView)
             myTextView.text = s
         }
     }
+
+
 }
