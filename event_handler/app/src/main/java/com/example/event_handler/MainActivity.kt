3b951637e69d3019e7f5f30f60be7a0c13e956e8
JuHan Park <iiiamst16@gmail.com>
2023-10-18 23:02:09 +0900

    Activity implements OnClickListener

--- event_handler/app/src/main/java/com/example/event_handler/MainActivity.kt
+++ event_handler/app/src/main/java/com/example/event_handler/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.event_handler
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val myButton = findViewById<Button>(R.id.button)
-        myButton.setOnClickListener {
-            val s: String = "안녕, 안드로이드 13!"
-            var myTextView = findViewById<TextView>(R.id.textView)
-            myTextView.text = s
-        }
+        myButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val s: String = "안녕, 안드로이드 13!"
+        var myTextView = findViewById<TextView>(R.id.textView)
+        myTextView.text = s
     }
 
 
