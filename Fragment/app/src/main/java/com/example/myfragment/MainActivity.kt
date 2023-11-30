f5323fd64a127835ceb5cdb5813f8116390d84d3
JuHan Park <iiiamst16@gmail.com>
2023-11-30 23:44:41 +0900

    active Fragment

--- Fragment/app/src/main/java/com/example/myfragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/example/myfragment/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.example.myfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentA,FragmentA())
+            commit()
+        }
     }
 }
