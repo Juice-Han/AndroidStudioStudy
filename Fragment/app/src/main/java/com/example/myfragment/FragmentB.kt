915d423b477434ef4d79269b536f255b44cc2f16
JuHan Park <iiiamst16@gmail.com>
2023-12-02 23:49:02 +0900

    active Fragment change

--- Fragment/app/src/main/java/com/example/myfragment/FragmentB.kt
+++ Fragment/app/src/main/java/com/example/myfragment/FragmentB.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myfragment.databinding.FragmentABinding
 
-class FragmentA : Fragment() {
+class FragmentB : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentABinding.inflate(inflater,container,false)
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false)
     }
+
+
 }
--- Fragment/app/src/main/java/com/example/myfragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/example/myfragment/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.myfragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentTransaction
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.myfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +12,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction().apply {
-            add(R.id.fragmentA,FragmentA())
-            commit()
+        val fragment: Fragment? =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if(fragment == null){
+            supportFragmentManager.beginTransaction().apply {
+                add(R.id.fragment_container,FragmentA())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.button.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, FragmentA())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.button2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.fragment_container, FragmentB())
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 }
